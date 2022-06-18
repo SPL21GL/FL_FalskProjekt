a0f8f56a375a7e16008827108d9bbaa4c68327fc
BoboLeBoo <lorenz.fritz@ms.bulme.at>
2022-06-18 10:28:53 +0200

    n zu n versuch3

--- controller/schule_lehrer.py
+++ controller/schule_lehrer.py
@@ -15,33 +15,42 @@ orderdetails_blueprint = Blueprint('orderdetails_blueprint', __name__)
 def add_teacher():
     school_Id = int(request.args["school_Id"])
     session: sqlalchemy.orm.scoping.scoped_session = db.session
+    add_order_form = OrderdetailForm()
+
     session.query(Lehrer).order_by(Lehrer.Lehrer_Id).all()
 
-    Lehrer.Lehrer_Id.choices = [(c.Lehrer_Id, c.Nachname) for c in Lehrer.Lehrer_Id.query.all()]
+    lehrer = session.query(Lehrer).order_by(Lehrer.Lehrer_Id).all()
+
+    product_list = [(str(p.Lehrer_Id), p.Vorname)
+                for p in Lehrer]
+
+    Lehrer.Lehrer_Id.choices = [(c.Lehrer_Id, c.Nachname) for c in session.query(Lehrer).order_by(Lehrer.Lehrer_Id).all()
+]   
+    add_order_form.Lehrer_Id.choices = product_list
 
     return redirect('schuleLehrer/schuleLehreradd.html')
 
 
 
-@orderdetails_blueprint.route("/orderdetails/delete", methods=["post"])
-def deleteorder():
-    delete_item_form_obj = OrderdetailDeleteForm()
-    if delete_item_form_obj.validate_on_submit():
+# @orderdetails_blueprint.route("/orderdetails/delete", methods=["post"])
+# def deleteorder():
+#     delete_item_form_obj = OrderdetailDeleteForm()
+#     if delete_item_form_obj.validate_on_submit():
 
-        order_number_to_delete = int(delete_item_form_obj.Lehrer_Id.data)
-        product_code_to_delete = (delete_item_form_obj.productCode.data)
-        orderdetail_to_delete = db.session.query(SchuleLehrer).filter(
-            sqlalchemy.and_(
-                SchuleLehrer.Nachname == order_number_to_delete,
+#         order_number_to_delete = int(delete_item_form_obj.Lehrer_Id.data)
+#         product_code_to_delete = (delete_item_form_obj.productCode.data)
+#         orderdetail_to_delete = db.session.query(SchuleLehrer).filter(
+#             sqlalchemy.and_(
+#                 SchuleLehrer.Nachname == order_number_to_delete,
 
-        ))
-        orderdetail_to_delete.delete()
+#         ))
+#         orderdetail_to_delete.delete()
 
-        db.session.commit()
-    else:
-        print("Fatal Error")
+#         db.session.commit()
+#     else:
+#         print("Fatal Error")
 
-    flash(f"Product with id {product_code_to_delete} has been deleted")
+#     flash(f"Product with id {product_code_to_delete} has been deleted")
 
-    return redirect("/lehrer/edit?Lehrer_Id=" + str(order_number_to_delete))
+#     return redirect("/lehrer/edit?Lehrer_Id=" + str(order_number_to_delete))
 
